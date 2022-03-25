c96965f18bc7bd79aeb2023c5614997cbb76e0b2
Hryhorii Khyzhniakov <soults15@gmail.com>
2022-03-25 17:52:44 +0000

    navbar component

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../main.scss';
+import Navbar from "../layout/navbar";
 
-const App: React.FC = () => <div>React app</div>;
+const App: React.FC = () => <Navbar />;
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from "./components/app";
 
 const appContainer = document.getElementById('app');
 
-appContainer && render(<App />, appContainer);
+appContainer && render(
+    <ChakraProvider resetCSS>
+        <App />
+    </ChakraProvider>,
+    appContainer
+);
